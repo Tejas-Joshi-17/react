94c5f801a7257cc6b2b7c58b20e728092f37082d
Tejas-Joshi-28 <tejas.joshi@sarvatra.in>
2025-12-19 03:44:38 +0530

    learning-props updated

--- learning-props/src/components/Card.jsx
+++ learning-props/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
                 <img src="./vite.svg" className="card-img-top" alt="vite logi"/>
                 <div className="card-body">
                 <h5 className="card-title">{props.name || "Deafult Value"}</h5>
-                <p className="card-text">Some quick example {props.name}F dsfds of the card’s content.</p>
+                <p className="card-text">{props.age || 12}</p>
                 <a target='_blank' href="www.google.com" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
--- learning-props/src/components/CardService.jsx
+++ learning-props/src/components/CardService.jsx
@@ -1,20 +1,31 @@
 
 import Card from './Card';
 
+const sampleObject = [
+  {
+    name : "sachin tencudule",
+    age : 23
+  },
+    {
+    name : "virat kohli",
+    age : 234
+  },
+    {
+    name : "dhobi",
+    age : 232
+  }
+]
+
 function CardService() {
 
   return (
     <div className='container text-center ml-5'>
       <div className='row mt-5'>
-        <div className="col">
-          <Card name="Sample"/>
-        </div>
-        <div className="col">
-          <Card/>
-        </div>
-        <div className="col">
-          <Card/>
-        </div>
+        {sampleObject.map((player, index) => (
+          <div className="col" key={index}>
+            <Card name={player.name} age={player.age} />
+          </div>
+        ))}
       </div>
     </div>
 
